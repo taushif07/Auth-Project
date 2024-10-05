471c327210859b3d786936b12995fc3486e0e86c
Taushif Ansari <taushif@ezygentechnology.com>
2024-10-05 22:53:18 +0530

    clean up code

--- my-app/src/aws-config.ts
+++
@@ -1,7 +0,0 @@
-// export const awsConfig = {
-//     region: import.meta.env.VITE_AWS_REGION,
-//     userPoolId: import.meta.env.VITE_AWS_USER_POOL_ID,
-//     userPoolWebClientId: import.meta.env.VITE_AWS_USER_POOL_WEB_CLIENT_ID,
-//   };
-  
-
--- my-app/src/config/awsConfig.ts
+++ my-app/src/config/awsConfig.ts
@@ -1,12 +1,3 @@
-// Dev
-// export const awsConfig = {
-//     identityPoolId: '',
-//     region: 'ap-south-1',
-//     userPoolId: 'ap-south-1_Yiuj6zWlg',
-//     userPoolWebClientId: '6j6nbv1u87t6hr7f1v4grktpnh'
-// };
-
-// QA
 export const awsConfig = {
   region: "us-east-1",
   userPoolId: `us-east-1_IXiXJ2HXb`,
